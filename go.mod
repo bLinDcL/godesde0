695ec5c2aa191c3d3ae53c9ef30cad643067139d
Felipe Vergara Albornoz <fevergara@entel.cl>
2025-08-07 19:55:08 -0400

    Version inicial

---
+++ go.mod
@@ -0,0 +1,3 @@
+module github.com/bLinDcL/godesde0
+
+go 1.24.6
---
+++ main.go
@@ -0,0 +1,9 @@
+package main
+
+import (
+	"github.com/bLinDcL/godesde0/variables"
+)
+
+func main() {
+	variables.MuestraEnteros()
+}
