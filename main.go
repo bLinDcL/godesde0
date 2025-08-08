0bae6dbbe125882baf20ffcc195a789d2e4fe436
Felipe Vergara Albornoz <fevergara@entel.cl>
2025-08-07 23:49:39 -0400

    Estructuras de control if y switch

--- main.go
+++ main.go
@@ -2,13 +2,28 @@ package main
 
 import (
 	"fmt"
+	"runtime"
 
 	"github.com/bLinDcL/godesde0/variables"
 )
 
 func main() {
 	estado, texto := variables.ConvertToText(1993)
-
 	fmt.Println(estado)
 	fmt.Println(texto)
+
+	if os := runtime.GOOS; os == "darwin" || os == "Linux." {
+		fmt.Println("OS X")
+	} else {
+		fmt.Println("Windows")
+	}
+
+	switch os := runtime.GOOS; os {
+	case "darwin":
+		fmt.Println("OS X")
+	case "Linux":
+		fmt.Println("Linux")
+	default:
+		fmt.Println("Windows")
+	}
 }
